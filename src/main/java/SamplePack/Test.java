520292ee0b941f81a6ad2e60281a424a80c0dd8c
VishnuVardhan-T <tssvishnu001@gmail.com>
2023-01-07 14:54:15 +0530

    Test is newly added

---
+++ src/main/java/SamplePack/Test.java
@@ -0,0 +1,10 @@
+package SamplePack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Another class");
+
+	}
+
+}
---
+++ src/main/java/SamplePack/Test2.java
@@ -0,0 +1,8 @@
+package SamplePack;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("updating this");
+	}
+}
